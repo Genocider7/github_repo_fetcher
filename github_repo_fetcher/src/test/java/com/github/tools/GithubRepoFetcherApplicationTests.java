22f83cbf1914885ba4eb844c18db950b1a3885b8
Filip <blazerwarrior@gmail.com>
2025-07-18 20:24:16 +0200

    Initial commit - Spring App files

---
+++ github_repo_fetcher/src/test/java/com/github/tools/GithubRepoFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
